6487fa29af144cb71fa196316fe06decc56da27b
Nick Battjes <nhbattjes@gmail.com>
2018-09-26 15:32:47 -0700

    now with unsafe dbs

--- main.rb
+++ main.rb
@@ -1,14 +1,26 @@
 require_relative 'framework'
+require_relative 'database'
+require_relative 'queries'
+
+DB = Database.connect('postgres://localhost/framework_dev', QUERIES)
+
 
 APP = App.new do 
-    get '/:foo' do |params|
-        params.fetch("foo")
+    get '/' do |params|
+        "hi root"
     end 
 
     get '/users/:username' do |params|
         "this is a #{params.fetch('username')}!"
     end 
     
+    get '/submissions' do |params|
+        DB.all_submissions
+    end 
+
+    get '/submissions/:name' do |params|
+        name = params.fetch('name')
+        user = DB.find_submission_by_name(name).fetch(0)
+        "the user is #{user.fetch('name')}"
+    end 
 end 
-
-
